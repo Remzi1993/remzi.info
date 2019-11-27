c6799d45d1aae2b5f018527282f745bcf390a476
Remzi <ict@remzi.info>
2019-11-27 17:37:53 +0100

    Tweaked settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,19 @@
 module.exports = {
 	siteMetadata: {
-		title: 'Gatsby Bulma Quickstart',
-		author: 'Aman Mittal',
-		imageUrl: 'https://i.imgur.com/Vz81GEl.png',
-		description: 'A Project to bootstrap your next Gatsby + Bulma site.',
+		title: `Gatsby Bulma Quickstart`,
+		author: `Aman Mittal`,
+		imageUrl: `https://i.imgur.com/Vz81GEl.png`,
+		description: `A Project to bootstrap your next Gatsby + Bulma site.`,
 		keywords: `Web developer, Web, Developer, CSS, HTML, JS, Javascript, Gatsby, Bulma Developer, CSS3, HTML5, Seo, Starter`,
-		twitter: 'https://twitter.com/amanhimself',
+		twitter: `https://twitter.com/amanhimself`,
 		github: `https://github.com/amandeepmittal`,
-		medium: 'https://medium.com/@amanhimself',
-		gatsby: 'https://www.gatsbyjs.org/',
-		bulma: 'https://bulma.io/',
+		medium: `https://medium.com/@amanhimself`,
+		gatsby: `https://www.gatsbyjs.org/`,
+		bulma: `https://bulma.io/`,
 		siteUrl: `https://www.example.com`
 	},
 	plugins: [
-		'gatsby-plugin-react-helmet',
+		`gatsby-plugin-react-helmet`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
@@ -21,25 +21,26 @@ module.exports = {
 				path: `${__dirname}/src/images`
 			}
 		},
-		'gatsby-transformer-sharp',
-		'gatsby-plugin-sharp',
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-sharp`,
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
-				name: 'Makefolio',
-				short_name: 'Makefolio',
-				start_url: '/',
-				background_color: '#2980b9',
-				theme_color: '#2980b9',
-				display: 'standalone',
-				icon: 'src/images/gatsby-icon.png',
-				orientation: 'portrait'
+				name: `Makefolio`,
+				short_name: `Makefolio`,
+				start_url: `/`,
+				background_color: `#2980b9`,
+				theme_color: `#2980b9`,
+				display: `standalone`,
+				icon: `src/images/gatsby-icon.png`,
+				orientation: `portrait`
 			}
 		},
 		`gatsby-plugin-sass`,
 		`gatsby-plugin-sitemap`,
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.app/offline
-		'gatsby-plugin-offline',
+		`gatsby-plugin-offline`,
+		`gatsby-plugin-netlify`
 	]
 };
