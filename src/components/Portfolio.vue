2c52c235a5a2d8b19a9058c0189fc8b035f95fd2
Remzi Cavdar <ict@remzi.info>
2023-03-20 19:02:44 +0100

    Small bugfixes

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -15,7 +15,7 @@
         </div>
       </div>
     </div>
-    <div class="px-4 pt-5 my-5 text-center border-bottom">
+    <div class="px-4 my-5 text-center border-bottom">
       <h1 class="display-4 fw-bold">Literatuuronderzoek</h1>
       <div class="col-lg-6 mx-auto">
         <p class="lead mb-4">De hypothese dat dwangmatig gebruik van sociale media tot negatieve consequenties kan
@@ -34,7 +34,9 @@
       </div>
       <div class="overflow-hidden" style="max-height: 30vh;">
         <div class="container px-5">
-          <img src="../assets/img/cover.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+          <a href="/assets/Onderzoeksverslag-van-R.Cavdar-20-01-2023.pdf" download="Onderzoeksverslag-van-R.Cavdar-20-01-2023.pdf">
+            <img src="../assets/img/cover.png" class="img-fluid border rounded-3 shadow-lg mb-4" alt="Example image" width="700" height="500" loading="lazy">
+          </a>
         </div>
       </div>
     </div>
