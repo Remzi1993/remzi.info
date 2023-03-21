a469a129f02e82800474f22956cbc143cc65984c
Remzi Cavdar <ict@remzi.info>
2023-03-22 00:42:50 +0100

    Small bugfix navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,34 +4,34 @@
       <router-link class="navbar-brand" to="/">
         <img src="/logo.png" alt="Logo">
       </router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu-navbar"
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <div class="collapse navbar-collapse" id="main-menu-navbar">
+        <ul class="navbar-nav me-auto my-2 mb-lg-0">
           <li class="nav-item">
             <router-link class="nav-link d-lg-none" to="/">
-              <span data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">Home</span>
+              <span data-bs-toggle="collapse" data-bs-target="#main-menu-navbar">Home</span>
             </router-link>
             <router-link class="nav-link d-none d-lg-block" to="/">Home</router-link>
           </li>
           <li class="nav-item">
             <router-link class="nav-link d-lg-none" to="/portfolio">
-              <span data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">Portfolio</span>
+              <span data-bs-toggle="collapse" data-bs-target="#main-menu-navbar">Portfolio</span>
             </router-link>
             <router-link class="nav-link d-none d-lg-block" to="/portfolio">Portfolio</router-link>
           </li>
           <li class="nav-item">
             <router-link class="nav-link d-lg-none" to="/cv">
-              <span data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">CV</span>
+              <span data-bs-toggle="collapse" data-bs-target="#main-menu-navbar">CV</span>
             </router-link>
             <router-link class="nav-link d-none d-lg-block" to="/cv">CV</router-link>
           </li>
           <li class="nav-item">
             <router-link class="nav-link d-lg-none" to="/contact">
-              <span data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">Contact</span>
+              <span data-bs-toggle="collapse" data-bs-target="#main-menu-navbar">Contact</span>
             </router-link>
             <router-link class="nav-link d-none d-lg-block" to="/contact">Contact</router-link>
           </li>
@@ -50,5 +50,14 @@ export default {
 <style lang="scss" scoped>
 .navbar-brand img {
   max-width: 30px;
+}
+
+#main-menu-navbar > ul > li > a.nav-link {
+  --bs-nav-link-padding-y: 0;
+}
+
+#main-menu-navbar > ul > li > a.nav-link > span {
+  display: block !important;
+  padding: 0.5rem 0;
 }
 </style>
