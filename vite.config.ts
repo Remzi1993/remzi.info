edba001c01092e16f19c09b662c0a43f2a3bcc09
Remzi Cavdar <ict@remzi.info>
2023-03-22 03:22:52 +0100

    Revert to stable state

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path'
 import {VitePWA} from 'vite-plugin-pwa'
-import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +12,6 @@ export default defineConfig({
     },
     plugins: [
         vue(),
-        VitePWA({registerType: 'autoUpdate'}),
-        viteCompression({ verbose: false, threshold: 100, algorithm: 'brotliCompress', deleteOriginFile: false }),
+        VitePWA({registerType: 'autoUpdate'})
     ],
 })
