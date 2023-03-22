98cef9a12f3b4dc228b909a7ccdd417a87eeed31
Remzi Cavdar <ict@remzi.info>
2023-03-22 01:44:26 +0100

    Added scrollBehavior

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,10 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(){
+        // always scroll to top
+        return { top: 0 }
+    },
 })
 
 createApp(App)
