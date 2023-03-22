e384dc08e9a430679243c9424df339c8c11dad76
Remzi Cavdar <ict@remzi.info>
2023-03-22 03:10:59 +0100

    Config compression

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
     plugins: [
         vue(),
         VitePWA({registerType: 'autoUpdate'}),
-        viteCompression()
+        viteCompression({ verbose: false, threshold: 100, algorithm: 'brotliCompress', deleteOriginFile: true }),
     ],
 })
