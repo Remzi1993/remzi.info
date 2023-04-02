22abcfee1e413f4e7522e212c868d8b82fdcb04a
Remzi Cavdar <ict@remzi.info>
2023-04-02 16:19:56 +0200

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path'
 import {VitePWA} from 'vite-plugin-pwa'
+import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
     },
     plugins: [
         vue(),
-        VitePWA({registerType: 'autoUpdate'})
+        VitePWA({registerType: 'autoUpdate'}),
+        checker({
+            typescript: true,
+        }),
     ],
 })
