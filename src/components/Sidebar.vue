41e7c8c8e5c95c1cdb0dae355d9076359eae56a2
Remzi Cavdar <ict@remzi.info>
2023-04-05 18:09:21 +0200

    Fixed link to cv

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,7 +5,7 @@
       <router-link class="btn btn-primary" to="/portfolio">Portfolio</router-link>
     </p>
     <p>
-      <a class="btn btn-primary" href="/assets/Curriculum-vitae–R.Cavdar.pdf"
+      <a class="btn btn-primary" href="https://assets.remzi.info/Curriculum-vitae-R.Cavdar.pdf"
          role="button" download="Curriculum-vitae–R.Cavdar.pdf">Download cv</a>
     </p>
 
