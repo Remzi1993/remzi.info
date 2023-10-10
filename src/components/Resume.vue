24d760b67a87fec1b91e396236df0586b7025840
Remzi Cavdar <ict@remzi.info>
2023-10-10 16:36:21 +0200

    Small changes

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="py-4">
+  <main class="py-4 small">
     <div class="container">
       <div class="row d-print-none">
         <div class="col">
@@ -7,47 +7,98 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-md-3">
-          <img src="../assets/img/profile-photo.jpg" alt="Remzi Cavdar" class="mb-3">
-          <p>
-            <strong><i class="bi bi-phone"></i> Mobiel:</strong> <a href="tel:+31611719929">06 117 199 29</a><br>
-            <strong><i class="bi bi-envelope-at"></i> E-mail:</strong> <a
-              href="mailto:ict@remzi.info">ict@remzi.info</a><br>
-            <strong><i class="bi bi-house"></i> Woonplaats:</strong> Amsterdam<br>
-          </p>
+        <div class="col-md-4">
+          <div class="card mb-3" style="width: 100%;">
+            <img src="../assets/img/profile-photo.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h6 class="card-title">Profiel</h6>
+              <p class="card-text small">Ik heb gevoel voor rechtvaardigheid en ben maatschappelijk betrokken.
+                Ook vind ik het belangrijk om eerlijk naar mijzelf en naar anderen te zijn.</p>
+              <p class="small">
+                <strong><i class="bi bi-phone"></i> Mobiel:</strong> <a href="tel:+31611719929">06 117 199 29</a><br>
+                <strong><i class="bi bi-envelope-at"></i> E-mail:</strong> <a
+                  href="mailto:ict@remzi.info">ict@remzi.info</a><br>
+                <strong><i class="bi bi-house"></i> Woonplaats:</strong> Amsterdam<br>
+                <strong><i class="bi bi-linkedin"></i> LinkedIn:</strong> <a
+                  href="https://www.linkedin.com/in/remzicavdar" target="_blank">linkedin.com/in/remzicavdar</a><br>
+                <strong><i class="bi bi-github"></i> GitHub:</strong> <a href="https://github.com/Remzi1993"
+                                                                         target="_blank">github.com/Remzi1993</a>
+              </p>
+            </div>
+          </div>
           <h4 class="red">Skills</h4>
           <div class="skill-rating">
-            <span>Web Dev</span>
+            <span>Nederlands</span>
             <div class="rating">
-              <input type="radio" name="skill-1" id="skill-1-star5"><label for="skill-1-star5" title="5 stars">★</label>
-              <input type="radio" name="skill-1" id="skill-1-star4" checked><label for="skill-1-star4" title="4 stars">★</label>
-              <input type="radio" name="skill-1" id="skill-1-star3"><label for="skill-1-star3" title="3 stars">★</label>
-              <input type="radio" name="skill-1" id="skill-1-star2"><label for="skill-1-star2" title="2 stars">★</label>
-              <input type="radio" name="skill-1" id="skill-1-star1"><label for="skill-1-star1" title="1 star">★</label>
+              <input type="radio" name="skill-6" id="skill-3-star5" disabled><label for="skill-6-star5" title="5 stars">★</label>
+              <input type="radio" name="skill-6" id="skill-3-star4" disabled checked><label for="skill-6-star4" title="4 stars">★</label>
+              <input type="radio" name="skill-6" id="skill-3-star3" disabled><label for="skill-6-star3" title="3 stars">★</label>
+              <input type="radio" name="skill-6" id="skill-3-star2" disabled><label for="skill-6-star2" title="2 stars">★</label>
+              <input type="radio" name="skill-6" id="skill-3-star1" disabled><label for="skill-6-star1" title="1 star">★</label>
+            </div>
+          </div>
+          <div class="skill-rating">
+            <span>Engels</span>
+            <div class="rating">
+              <input type="radio" name="skill-7" id="skill-3-star5" disabled><label for="skill-7-star5" title="5 stars">★</label>
+              <input type="radio" name="skill-7" id="skill-3-star4" disabled><label for="skill-7-star4" title="4 stars">★</label>
+              <input type="radio" name="skill-7" id="skill-3-star3" disabled checked><label for="skill-7-star3" title="3 stars">★</label>
+              <input type="radio" name="skill-7" id="skill-3-star2" disabled><label for="skill-7-star2" title="2 stars">★</label>
+              <input type="radio" name="skill-7" id="skill-3-star1" disabled><label for="skill-7-star1" title="1 star">★</label>
+            </div>
+          </div>
+          <div class="skill-rating">
+            <span>Full stack web development</span>
+            <div class="rating">
+              <input type="radio" name="skill-1" id="skill-1-star5" disabled><label for="skill-1-star5" title="5 stars">★</label>
+              <input type="radio" name="skill-1" id="skill-1-star4" checked disabled><label for="skill-1-star4" title="4 stars">★</label>
+              <input type="radio" name="skill-1" id="skill-1-star3" disabled><label for="skill-1-star3" title="3 stars">★</label>
+              <input type="radio" name="skill-1" id="skill-1-star2" disabled><label for="skill-1-star2" title="2 stars">★</label>
+              <input type="radio" name="skill-1" id="skill-1-star1" disabled><label for="skill-1-star1" title="1 star">★</label>
             </div>
           </div>
           <div class="skill-rating">
             <span>Java</span>
             <div class="rating">
-              <input type="radio" name="skill-2" id="skill-2-star5"><label for="skill-2-star5" title="5 stars">★</label>
-              <input type="radio" name="skill-2" id="skill-2-star4"><label for="skill-2-star4" title="4 stars">★</label>
-              <input type="radio" name="skill-2" id="skill-2-star3" checked><label for="skill-2-star3" title="3 stars">★</label>
-              <input type="radio" name="skill-2" id="skill-2-star2"><label for="skill-2-star2" title="2 stars">★</label>
-              <input type="radio" name="skill-2" id="skill-2-star1"><label for="skill-2-star1" title="1 star">★</label>
+              <input type="radio" name="skill-2" id="skill-2-star5" disabled><label for="skill-2-star5" title="5 stars">★</label>
+              <input type="radio" name="skill-2" id="skill-2-star4" disabled><label for="skill-2-star4" title="4 stars">★</label>
+              <input type="radio" name="skill-2" id="skill-2-star3" checked disabled><label for="skill-2-star3" title="3 stars">★</label>
+              <input type="radio" name="skill-2" id="skill-2-star2" disabled><label for="skill-2-star2" title="2 stars">★</label>
+              <input type="radio" name="skill-2" id="skill-2-star1" disabled><label for="skill-2-star1" title="1 star">★</label>
             </div>
           </div>
           <div class="skill-rating">
             <span>React & Vue.js</span>
             <div class="rating">
-              <input type="radio" name="skill-3" id="skill-3-star5"><label for="skill-3-star5" title="5 stars">★</label>
-              <input type="radio" name="skill-3" id="skill-3-star4"><label for="skill-3-star4" title="4 stars">★</label>
-              <input type="radio" name="skill-3" id="skill-3-star3" checked><label for="skill-3-star3" title="3 stars">★</label>
-              <input type="radio" name="skill-3" id="skill-3-star2"><label for="skill-3-star2" title="2 stars">★</label>
-              <input type="radio" name="skill-3" id="skill-3-star1"><label for="skill-3-star1" title="1 star">★</label>
+              <input type="radio" name="skill-3" id="skill-3-star5" disabled><label for="skill-3-star5" title="5 stars">★</label>
+              <input type="radio" name="skill-3" id="skill-3-star4" disabled><label for="skill-3-star4" title="4 stars">★</label>
+              <input type="radio" name="skill-3" id="skill-3-star3" checked disabled><label for="skill-3-star3" title="3 stars">★</label>
+              <input type="radio" name="skill-3" id="skill-3-star2" disabled><label for="skill-3-star2" title="2 stars">★</label>
+              <input type="radio" name="skill-3" id="skill-3-star1" disabled><label for="skill-3-star1" title="1 star">★</label>
+            </div>
+          </div>
+          <div class="skill-rating">
+            <span>MySQL</span>
+            <div class="rating">
+              <input type="radio" name="skill-4" id="skill-3-star5" disabled><label for="skill-4-star5" title="5 stars">★</label>
+              <input type="radio" name="skill-4" id="skill-3-star4" disabled><label for="skill-4-star4" title="4 stars">★</label>
+              <input type="radio" name="skill-4" id="skill-3-star3" checked disabled><label for="skill-4-star3" title="3 stars">★</label>
+              <input type="radio" name="skill-4" id="skill-3-star2" disabled><label for="skill-4-star2" title="2 stars">★</label>
+              <input type="radio" name="skill-4" id="skill-3-star1" disabled><label for="skill-4-star1" title="1 star">★</label>
+            </div>
+          </div>
+          <div class="skill-rating">
+            <span>PHP & WordPress</span>
+            <div class="rating">
+              <input type="radio" name="skill-5" id="skill-3-star5" disabled><label for="skill-5-star5" title="5 stars">★</label>
+              <input type="radio" name="skill-5" id="skill-3-star4" disabled><label for="skill-5-star4" title="4 stars">★</label>
+              <input type="radio" name="skill-5" id="skill-3-star3" checked disabled><label for="skill-5-star3" title="3 stars">★</label>
+              <input type="radio" name="skill-5" id="skill-3-star2" disabled><label for="skill-5-star2" title="2 stars">★</label>
+              <input type="radio" name="skill-5" id="skill-3-star1" disabled><label for="skill-5-star1" title="1 star">★</label>
             </div>
           </div>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-8">
           <h2 class="text-center">Remzi Cavdar</h2>
           <h3 class="text-center yellow">Software Engineer</h3>
           <hr/>
@@ -103,40 +154,5 @@ export default {
 <style lang="scss" scoped>
 img {
   width: 100%;
-}
-
-.skill-rating {
-  display: flex;
-  align-items: center;
-  margin-bottom: 10px;
-  justify-content: space-between;
-
-  span {
-    margin-right: 10px;
-    font-weight: bold;
-  }
-}
-
-.rating {
-  display: flex;
-  direction: rtl; /* This will make the stars order from left to right */
-  font-size: 1.5em; /* Adjusting the size a bit */
-  justify-content: center;
-}
-
-.rating > input {
-  display: none;
-}
-
-.rating > label {
-  color: #555; /* Default star color for dark mode */
-  cursor: pointer;
-  padding: 0 5px;
-}
-
-.rating > input:checked ~ label,
-.rating > label:hover,
-.rating > label:hover ~ label {
-  color: #FFD700; /* Yellow star color for selected and hover */
 }
 </style>
