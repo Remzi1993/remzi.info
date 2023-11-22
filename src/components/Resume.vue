a8237f056ce4e2ea5f56779542b11ff84ea78123
Remzi Cavdar <ict@remzi.info>
2023-11-22 03:46:31 +0100

    Update

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="py-4 small">
+  <main class="py-4">
     <div class="container">
       <div class="row d-print-none">
         <div class="col pb-4">
@@ -8,19 +8,24 @@
                 role="button" download="Curriculum-vitae–R.Cavdar.pdf">Liever een PDF?</a></p>
         </div>
       </div>
+
+      <div class="d-none d-print-block pt-5"></div>
+
       <div class="row">
-        <div class="col-md-4">
-          <div class="card mb-3" style="width: 100%;">
-            <img src="../assets/img/profile-photo.jpg" class="card-img-top" alt="Profile image">
-            <div class="card-body">
-              <h6 class="card-title">Profiel</h6>
-              <p class="card-text small">Ik heb gevoel voor rechtvaardigheid en ben maatschappelijk betrokken.
-                Ook vind ik het belangrijk om eerlijk naar mijzelf en naar anderen te zijn.</p>
-              <p class="small">
+        <div class="col-md-6">
+          <div class="row">
+            <div class="col-md-4" id="profile-image">
+              <img src="../assets/img/profile-photo.jpg" alt="Profile image">
+            </div>
+            <div class="col-md-8">
+              <h2 class="mt-4 mt-md-0">Remzi Cavdar</h2>
+              <h3 class="yellow subtitle">Software Engineer</h3>
+              <p id="info">
+                <strong><i class="bi bi-calendar-date"></i> Geboortedatum:</strong> 11-06-1993<br>
+                <strong><i class="bi bi-house"></i> Woonplaats:</strong> Amsterdam<br>
                 <strong><i class="bi bi-phone"></i> Mobiel:</strong> <a href="tel:+31611719929">06 117 199 29</a><br>
                 <strong><i class="bi bi-envelope-at"></i> E-mail:</strong> <a
                   href="mailto:ict@remzi.info">ict@remzi.info</a><br>
-                <strong><i class="bi bi-house"></i> Woonplaats:</strong> Amsterdam<br>
                 <strong><i class="bi bi-linkedin"></i> LinkedIn:</strong> <a
                   href="https://www.linkedin.com/in/remzicavdar" target="_blank">linkedin.com/in/remzicavdar</a><br>
                 <strong><i class="bi bi-github"></i> GitHub:</strong> <a href="https://github.com/Remzi1993"
@@ -28,119 +33,156 @@
               </p>
             </div>
           </div>
-          <h4 class="red">Skills</h4>
-          <div class="skill-rating">
+
+          <h2 class="red mt-3 mt-lg-4">Opleiding</h2>
+          <h3>2021 - HEDEN - Hogeschool van Amsterdam</h3>
+          <h4 class="yellow subtitle">HBO-ICT Software Engineering</h4>
+          <h3>2011 - 2014 - ROC TOP</h3>
+          <h4 class="yellow subtitle">MBO niveau 4 / Applicatie- en mediaontwikkeling</h4>
+
+          <h2 class="red mt-3 mt-lg-4">Skills</h2>
+          <div class="rating">
             <span>Nederlands</span>
-            <div class="rating">
-              <input type="radio" id="skill-1-star5" disabled><label for="skill-1-star5" title="5 stars">★</label>
-              <input type="radio" id="skill-1-star4" disabled checked><label for="skill-1-star4" title="4 stars">★</label>
-              <input type="radio" id="skill-1-star3" disabled><label for="skill-1-star3" title="3 stars">★</label>
-              <input type="radio" id="skill-1-star2" disabled><label for="skill-1-star2" title="2 stars">★</label>
-              <input type="radio" id="skill-1-star1" disabled><label for="skill-1-star1" title="1 star">★</label>
+            <div class="stars">
+              <input type="radio" id="skill-1-star5" name="skill-1" disabled>
+              <label for="skill-1-star5"></label>
+              <input type="radio" id="skill-1-star4" name="skill-1" disabled checked>
+              <label for="skill-1-star4"></label>
+              <input type="radio" id="skill-1-star3" name="skill-1" disabled>
+              <label for="skill-1-star3"></label>
+              <input type="radio" id="skill-1-star2" name="skill-1" disabled>
+              <label for="skill-1-star2"></label>
+              <input type="radio" id="skill-1-star1" name="skill-1" disabled>
+              <label for="skill-1-star1"></label>
             </div>
           </div>
-          <div class="skill-rating">
+
+          <div class="rating">
             <span>Engels</span>
-            <div class="rating">
-              <input type="radio" id="skill-2-star5" disabled><label for="skill-2-star5" title="5 stars">★</label>
-              <input type="radio" id="skill-2-star4" disabled><label for="skill-2-star4" title="4 stars">★</label>
-              <input type="radio" id="skill-2-star3" disabled checked><label for="skill-2-star2" title="3 stars">★</label>
-              <input type="radio" id="skill-2-star2" disabled><label for="skill-2-star1" title="2 star">★</label>
-              <input type="radio" id="skill-2-star1" disabled><label for="skill-2-star3" title="1 stars">★</label>
+            <div class="stars">
+              <input type="radio" id="skill-2-star5" name="skill-2" disabled>
+              <label for="skill-2-star5"></label>
+              <input type="radio" id="skill-2-star4" name="skill-2" disabled>
+              <label for="skill-2-star4"></label>
+              <input type="radio" id="skill-2-star3" name="skill-2" disabled checked>
+              <label for="skill-2-star3"></label>
+              <input type="radio" id="skill-2-star2" name="skill-2" disabled>
+              <label for="skill-2-star2"></label>
+              <input type="radio" id="skill-2-star1" name="skill-2" disabled>
+              <label for="skill-2-star1"></label>
             </div>
           </div>
-          <div class="skill-rating">
+
+          <div class="rating">
             <span>Full stack web development</span>
-            <div class="rating">
-              <input type="radio" id="skill-3-star5" disabled><label for="skill-3-star5" title="5 stars">★</label>
-              <input type="radio" id="skill-3-star4" checked disabled><label for="skill-3-star4" title="4 stars">★</label>
-              <input type="radio" id="skill-3-star3" disabled><label for="skill-3-star3" title="3 stars">★</label>
-              <input type="radio" id="skill-3-star2" disabled><label for="skill-3-star2" title="2 stars">★</label>
-              <input type="radio" id="skill-3-star1" disabled><label for="skill-3-star1" title="1 star">★</label>
+            <div class="stars">
+              <input type="radio" id="skill-3-star5" name="skill-3" disabled>
+              <label for="skill-3-star5"></label>
+              <input type="radio" id="skill-3-star4" name="skill-3" disabled checked>
+              <label for="skill-3-star4"></label>
+              <input type="radio" id="skill-3-star3" name="skill-3" disabled>
+              <label for="skill-3-star3"></label>
+              <input type="radio" id="skill-3-star2" name="skill-3" disabled>
+              <label for="skill-3-star2"></label>
+              <input type="radio" id="skill-3-star1" name="skill-3" disabled>
+              <label for="skill-3-star1"></label>
             </div>
           </div>
-          <div class="skill-rating">
+
+          <div class="rating">
             <span>Java</span>
-            <div class="rating">
-              <input type="radio" id="skill-4-star5" disabled><label for="skill-4-star5" title="5 stars">★</label>
-              <input type="radio" id="skill-4-star4" disabled><label for="skill-4-star4" title="4 stars">★</label>
-              <input type="radio" id="skill-4-star3" checked disabled><label for="skill-4-star3" title="3 stars">★</label>
-              <input type="radio" id="skill-4-star2" disabled><label for="skill-4-star2" title="2 stars">★</label>
-              <input type="radio" id="skill-4-star1" disabled><label for="skill-4-star1" title="1 star">★</label>
+            <div class="stars">
+              <input type="radio" id="skill-4-star5" name="skill-4" disabled>
+              <label for="skill-4-star5"></label>
+              <input type="radio" id="skill-4-star4" name="skill-4" disabled>
+              <label for="skill-4-star4"></label>
+              <input type="radio" id="skill-4-star3" name="skill-4" disabled checked>
+              <label for="skill-4-star3"></label>
+              <input type="radio" id="skill-4-star2" name="skill-4" disabled>
+              <label for="skill-4-star2"></label>
+              <input type="radio" id="skill-4-star1" name="skill-4" disabled>
+              <label for="skill-4-star1"></label>
             </div>
           </div>
-          <div class="skill-rating">
+
+          <div class="rating">
             <span>React & Vue.js</span>
-            <div class="rating">
-              <input type="radio" id="skill-5-star5" disabled><label for="skill-5-star5" title="5 stars">★</label>
-              <input type="radio" id="skill-5-star4" disabled><label for="skill-5-star4" title="4 stars">★</label>
-              <input type="radio" id="skill-5-star3" checked disabled><label for="skill-5-star3" title="3 stars">★</label>
-              <input type="radio" id="skill-5-star2" disabled><label for="skill-5-star2" title="2 stars">★</label>
-              <input type="radio" id="skill-5-star1" disabled><label for="skill-5-star1" title="1 star">★</label>
+            <div class="stars">
+              <input type="radio" id="skill-5-star5" name="skill-5" disabled>
+              <label for="skill-5-star5"></label>
+              <input type="radio" id="skill-5-star4" name="skill-5" disabled>
+              <label for="skill-5-star4"></label>
+              <input type="radio" id="skill-5-star3" name="skill-5" disabled checked>
+              <label for="skill-5-star3"></label>
+              <input type="radio" id="skill-5-star2" name="skill-5" disabled>
+              <label for="skill-5-star2"></label>
+              <input type="radio" id="skill-5-star1" name="skill-5" disabled>
+              <label for="skill-5-star1"></label>
             </div>
           </div>
-          <div class="skill-rating">
+
+          <div class="rating">
             <span>MySQL</span>
-            <div class="rating">
-              <input type="radio" id="skill-6-star5" disabled><label for="skill-6-star5" title="5 stars">★</label>
-              <input type="radio" id="skill-6-star4" disabled><label for="skill-6-star4" title="4 stars">★</label>
-              <input type="radio" id="skill-6-star3" checked disabled><label for="skill-6-star3" title="3 stars">★</label>
-              <input type="radio" id="skill-6-star2" disabled><label for="skill-6-star2" title="2 stars">★</label>
-              <input type="radio" id="skill-6-star1" disabled><label for="skill-6-star1" title="1 star">★</label>
+            <div class="stars">
+              <input type="radio" id="skill-6-star5" name="skill-6" disabled>
+              <label for="skill-6-star5"></label>
+              <input type="radio" id="skill-6-star4" name="skill-6" disabled>
+              <label for="skill-6-star4"></label>
+              <input type="radio" id="skill-6-star3" name="skill-6" disabled checked>
+              <label for="skill-6-star3"></label>
+              <input type="radio" id="skill-6-star2" name="skill-6" disabled>
+              <label for="skill-6-star2"></label>
+              <input type="radio" id="skill-6-star1" name="skill-6" disabled>
+              <label for="skill-6-star1"></label>
             </div>
           </div>
-          <div class="skill-rating">
+
+          <div class="rating">
             <span>PHP & WordPress</span>
-            <div class="rating">
-              <input type="radio" id="skill-7-star5" disabled><label for="skill-7-star5" title="5 stars">★</label>
-              <input type="radio" id="skill-7-star4" disabled><label for="skill-7-star4" title="4 stars">★</label>
-              <input type="radio" id="skill-7-star3" checked disabled><label for="skill-7-star3" title="3 stars">★</label>
-              <input type="radio" id="skill-7-star2" disabled><label for="skill-7-star2" title="2 stars">★</label>
-              <input type="radio" id="skill-7-star1" disabled><label for="skill-7-star1" title="1 star">★</label>
+            <div class="stars">
+              <input type="radio" id="skill-7-star5" name="skill-7" disabled>
+              <label for="skill-7-star5"></label>
+              <input type="radio" id="skill-7-star4" name="skill-7" disabled>
+              <label for="skill-7-star4"></label>
+              <input type="radio" id="skill-7-star3" name="skill-7" disabled checked>
+              <label for="skill-7-star3"></label>
+              <input type="radio" id="skill-7-star2" name="skill-7" disabled>
+              <label for="skill-7-star2"></label>
+              <input type="radio" id="skill-7-star1" name="skill-7" disabled>
+              <label for="skill-7-star1"></label>
             </div>
           </div>
         </div>
-        <div class="col-md-8 ps-xl-4">
-          <h2 class="text-center">Remzi Cavdar</h2>
-          <h3 class="text-center yellow">Software Engineer</h3>
-          <hr/>
-          <h4 class="red">Werkervaring</h4>
-          <h5>Lead Me - Direct Leads - 02/2019 - 09/2019</h5>
-          <h6>Web Developer / WordPress Developer / Systeembeheerde</h6>
+        <div class="col-md-6">
+          <h2 class="red mt-3 mt-md-0">Werkervaring</h2>
+          <h3>2019 - Lead Me</h3>
+          <h4 class="yellow subtitle">WordPress Developer / Systeembeheerder</h4>
           <ul>
-            <li>Hosting / servers beheren (backups, instellingen, optimaliseren en kwaliteit bewaken)</li>
-            <li>Websites onderhouden, bijwerken (updaten) en testen voordat er (grote) aanpassingen worden doorgevoerd
-            </li>
-            <li>Bugs oplossen</li>
+            <li>Beheren van hosting (backups, optimaliseren en kwaliteit bewaken)</li>
+            <li>Onderhouden van WordPress websites</li>
+            <li>Ontwikkelen van oplossingen voor WordPress websites</li>
+            <li>Testen en bugs oplossen</li>
           </ul>
-          <h5>PREMIUMS / Advanza / Advanza Direct - 02/2018 - 02/2019</h5>
-          <h6>Web Developer / WordPress Developer / Systeembeheerder</h6>
+          <h3>2018 - 2019 - PREMIUMS / Advanza</h3>
+          <h4 class="yellow subtitle">Web Developer</h4>
           <ul>
-            <li>Binnen Advanza Direct: de hosting / servers beheren (backups, instellingen, optimaliseren en kwaliteit
-              bewaken)
-            </li>
-            <li>Binnen Advanza Direct: koppeling met WordPress met de Advanza backend, via WordPress plugin</li>
-            <li>Verantwoordelijk binnen Advanza Direct voor alle online formulieren</li>
-            <li>Binnen Advanza Direct: websites onderhouden en bijwerken</li>
-            <li>Binnen Advanza Direct: oplossen van problemen / bugs (fouten in WordPress plugins en thema’s)</li>
-            <li>Soms voor Premiums tussendoor IT problemen oplossen</li>
-            <li>Soms voor Advanza IT problemen oplossen</li>
+            <li>Ontwikkelen en onderhouden van WordPress plugins en thema's</li>
+            <li>Ontwikkelen en onderhouden van online formulieren</li>
+            <li>Ontwikkelen, bijwerken en onderhouden van websites</li>
+            <li>Oplossen van verschillende IT problemen</li>
           </ul>
-          <h5>Remzi.info / Freelance - 06/2012 - 07/2015</h5>
-          <h6>Full Stack Web Developer</h6>
+          <h3>2014 - 2015 - 10Forward</h3>
+          <h4 class="yellow subtitle">Web Developer</h4>
           <ul>
-            <li>Websites ontwikkelen (kleine / middelgrote projecten)</li>
-            <li>Technisch advies: cliënten adviseren over verschillende ICT-vraagstukken.</li>
-            <li>Webhosting verzorgen voor cliënten</li>
-            <li>Web Apps ontwikkelen</li>
-            <li>Ervaring Webhosting panels: cPanel en DirectAdmin en CMS: WordPress en Magento</li>
+            <li>Ontwikkelen, bijwerken en onderhouden van websites & webshops</li>
           </ul>
-          <hr/>
-          <h4 class="red">Opleiding</h4>
-          <h5>Hogeschool van Amsterdam - 09/2021 - heden</h5>
-          <h6>HBO-ICT Software Engineering</h6>
-          <h5>ROC TOP - 09/2011 - 09/2014</h5>
-          <h6>MBO niveau 4 / Applicatie- en mediaontwikkeling</h6>
+          <h3>2012 - 2015 - Remzi.info</h3>
+          <h4 class="yellow subtitle">Freelance Full Stack Web Developer & advies</h4>
+          <ul>
+            <li>Ontwikkelen, bijwerken en onderhouden van websites</li>
+            <li>Verzorgen van webhosting voor cliënten</li>
+            <li>Adviseren over verschillende ICT-vraagstukken.</li>
+          </ul>
         </div>
       </div>
     </div>
@@ -154,7 +196,66 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+#info {
+  margin: 0;
+}
+h1 {
+  font-size: 2rem;
+}
+
+h2 {
+  font-size: 1.5rem;
+}
+
+h3 {
+  font-size: 1.25rem;
+}
+
+h4 {
+  font-size: 1rem;
+}
+
+.subtitle {
+  margin-top: -0.6rem;
+}
+
+ul, p, .rating, .subtitle {
+  font-size: 1rem;
+}
+
 img {
   width: 100%;
+}
+
+@media (max-width: 575.98px) {
+  #profile-image {
+    display: flex;
+    justify-content: center;
+  }
+
+  img {
+    margin-bottom: 1rem;
+  }
+}
+
+@media (max-width: 767.98px) {
+  img {
+    width: 50%;
+  }
+}
+
+@media (max-width: 991.98px) {
+  h2 {
+    font-size: 17px;
+  }
+  h3, h4 {
+    font-size: 12px;
+  }
+  .subtitle {
+    margin-top: -0.6rem;
+  }
+  ul, p, .rating, .subtitle {
+    font-size: 11px;
+  }
 }
 </style>
