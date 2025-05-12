d269db2965f4efc77a4c5994846fe9aba3e6732e
Remzi Cavdar <ict@remzi.info>
2025-05-12 05:22:56 +0200

    Update

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -10,17 +10,17 @@
             <path fill="#35495e" d="M78.8,10L64,35.4L49.2,10H25.6L64,76l38.4-66H78.8z" data-v-c0161dce=""></path>
           </svg>
         </span>
-        en de code is te vinden op <a href="https://github.com/Remzi1993/remzi.info" target="_blank"><i
+        en de code is te vinden op <a href="https://github.com/Remzi1993/remzi.info" target="_blank" aria-label="GitHub profile of Remzi Cavdar"><i
           class="bi bi-github"></i></a>
 
         en ik maak gebruik van:
         <a href="https://dashboard.simpleanalytics.com/remzi.info?utm_source=remzi.info&utm_content=badge"
-           referrerpolicy="origin" target="_blank">
+           referrerpolicy="origin" target="_blank" aria-label="See public Simple Analytics dashboard">
           <picture>
             <source srcset="https://simpleanalyticsbadges.com/remzi.info?mode=dark"
                     media="(prefers-color-scheme: dark)"/>
             <img src="https://simpleanalyticsbadges.com/remzi.info?mode=light" loading="lazy"
-                 referrerpolicy="no-referrer" crossorigin="anonymous"/></picture>
+                 referrerpolicy="no-referrer" crossorigin="anonymous" alt="Logo of Simple Analytics"></picture>
         </a>
       </p>
     </div>
